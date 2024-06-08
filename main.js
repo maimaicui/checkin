080691e911fdf84409f0a18bb6b5677e150e9fc8
maimaicui <57396885+maimaicui@users.noreply.github.com>
2024-06-08 13:57:49 +0800

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const glados = async () => {
   if (!cookie) return
   try {
     const headers = {
+      
       'cookie': cookie,
       'referer': 'https://glados.rocks/console/checkin',
       'user-agent': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)',
